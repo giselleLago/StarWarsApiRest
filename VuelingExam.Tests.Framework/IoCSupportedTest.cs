a0544691d87bce134bdcaf4e7c88a186e5a409db
giselle <glj@holacons.com>
2020-03-18 15:49:22 +0100

    Add Test Framework.

---
+++ VuelingExam.Tests.Framework/IoCSupportedTest.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Core;
+
+namespace VuelingExam.Test.Framework
+{
+    public class IoCSuportedTest<TModule> where TModule : IModule, new()
+    {
+        private readonly IContainer container;
+        public IoCSuportedTest()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TModule());
+            container = builder.Build();
+        }
+
+        protected TEntity Resolve<TEntity>()
+        {
+            return container.Resolve<TEntity>();
+        }
+
+        protected void ShutdownIoC()
+        {
+            container.Dispose();
+        }
+    }
+}
