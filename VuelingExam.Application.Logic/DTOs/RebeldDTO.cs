9429746962e10dc71dcd2b63368f2e9595e44390
giselle <glj@holacons.com>
2020-03-18 16:53:06 +0100

    Add DTO to Application Logic.

---
+++ VuelingExam.Application.Logic/DTOs/RebeldDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VuelingExam.Application.Logic.DTOs
+{
+    public class RebeldDto
+    {
+        public IEnumerable<string> Name { get; set; }
+        public string PlanetName { get; set; }
+
+    }
+}
