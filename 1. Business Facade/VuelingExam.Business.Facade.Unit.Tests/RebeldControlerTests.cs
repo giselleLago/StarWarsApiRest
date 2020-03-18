e14824b2f675621360a33cbc3a2e42bfb22e8772
giselle <glj@holacons.com>
2020-03-18 19:59:46 +0100

    Add Unit Test

--- 1. Business Facade/VuelingExam.Business.Facade.Unit.Tests/RebeldControlerTests.cs
+++ 1. Business Facade/VuelingExam.Business.Facade.Unit.Tests/RebeldControlerTests.cs
@@ -3,6 +3,7 @@ using log4net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Web.Http;
 using System.Web.Http.Results;
 using VuelingExam.Application.Logic.Contracts;
 using VuelingExam.Application.Logic.DTOs;
@@ -28,9 +29,30 @@ namespace VuelingExam.Business.Facade.Unit.Tests
                 var mockedService = mock.Create<IService<RebeldDto>>();
                 var mockedLog = mock.Create<ILog>();
                 var controller = new RebeldController(mockedLog, mockedService);
-                var result = controller.Register(rebeldDto);
+                IHttpActionResult result = controller.Register(rebeldDto);
                 var response = result as OkNegotiatedContentResult<RebeldDto>;
                 Assert.AreEqual(rebeldDto, response.Content);
+            }
+        }
+
+        [TestMethod()]
+        public void RegisterTest1()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var rebeldDto = new RebeldDto
+                {
+                    Names = new List<String>() { ("Pepe") },
+                    PlanetName = "Saturno"
+                };
+                mock.Mock<IService<RebeldDto>>().Setup(repository => repository.Register(rebeldDto)).Returns(true);
+                mock.Mock<ILog>();
+                var mockedService = mock.Create<IService<RebeldDto>>();
+                var mockedLog = mock.Create<ILog>();
+                var controller = new RebeldController(mockedLog, mockedService);
+                IHttpActionResult result = controller.Register(rebeldDto);
+                var response = result as OkNegotiatedContentResult<RebeldDto>;
+                Assert.IsNotNull(response.Content);
             }
         }
     }
