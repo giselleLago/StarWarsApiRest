d9822947a8a4390e942439ef9ed86e4d9ebf18c4
giselle <glj@holacons.com>
2020-03-18 19:45:57 +0100

    Add RepositoryUnitTests.

---
+++ VuelingExam.Infrastructure.Repository.Unit.Tests/Implementations/RebeldRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Autofac.Extras.Moq;
+using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VuelingExam.Domain.Entities.Aggregate;
+using VuelingExam.Infrastructure.Repositories.Contracts;
+
+namespace VuelingExam.Infrastructure.Repository.Implementations.Unit.Tests
+{
+    [TestClass()]
+    public class RebeldRepositoryTests
+    {
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var rebeld = new Rebeld();
+                mock.Mock<IInfrastructureRepository<Rebeld>>().Setup(repository => repository.Register(rebeld)).Returns(rebeld);
+                mock.Mock<ILog>();
+                var mockedLog = mock.Create<ILog>();
+                var rebeldRepository = new RebeldRepository(mockedLog);
+                var result = rebeldRepository.Register(rebeld);
+                Assert.AreEqual(rebeld, result);
+            }
+        }
+    }
+}
